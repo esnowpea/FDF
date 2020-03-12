32df4c226a9c0b0ac1b7b4ffbea803167dbeec15
esnowpea <esnowpea@student.21-school.ru>
2020-03-12 12:31:11 +0300

    final 3

---
+++ cmake-build-debug/CMakeFiles/fdf_new.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fdf_new.dir/src/event_handler.c.o"
+  "CMakeFiles/fdf_new.dir/src/ft_atoi_base.c.o"
+  "CMakeFiles/fdf_new.dir/src/hook.c.o"
+  "CMakeFiles/fdf_new.dir/src/main.c.o"
+  "CMakeFiles/fdf_new.dir/src/mlx_put_line.c.o"
+  "CMakeFiles/fdf_new.dir/src/print_image.c.o"
+  "CMakeFiles/fdf_new.dir/src/projection.c.o"
+  "CMakeFiles/fdf_new.dir/src/read_map.c.o"
+  "fdf_new"
+  "fdf_new.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fdf_new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
